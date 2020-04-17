f8948759aceb9193b2d486389707b60a29efca6c
SanujaLW <sanujalw@gmail.com>
2020-04-17 16:27:00 +0530

    Updated did-siop.bundle

--- src/app/did-siop.service.ts
+++ src/app/did-siop.service.ts
@@ -7,11 +7,13 @@ import * as DID_SIOP from '../assets/did-siop.bundle.js';
 export class DidSiopService {
 
   private siop_rp: any;
+  private siop_user: any;
+
   constructor() { 
     this.siop_rp = new DID_SIOP.RP();
 
     this.siop_rp.initialize(
-      'localhost:8080/loginCallback.html', // RP redirect uri
+      'localhost:4200/home', // RP redirect uri
       'did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83',// RP DID
       {
         "jwks_uri": "https://uniresolver.io/1.0/identifiers/did:example:0xab;transform-keys=jwks",
@@ -24,9 +26,19 @@ export class DidSiopService {
       'did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83#owner',// RP public key uri for kid
       'ES256K-R'// Algorithm
     );
+
+    /* this.siop_rp.setSigningParams(
+      'CE438802C1F0B6F12BC6E686F372D7D495BC5AA634134B4A7EA4603CB25F0965', //Invalid RP private key
+      'did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83#owner',// RP public key uri for kid
+      'ES256K-R'// Algorithm
+    ); */
   }
 
-  getRP(){
-    return this.siop_rp;
+  async getRequest(): Promise<string>{
+    return await this.siop_rp.generateRequest();
+  }
+
+  async processResponse(response: string): Promise<any>{
+    return await this.siop_rp.validateResponse(response);
   }
 }
