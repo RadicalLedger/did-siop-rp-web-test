095ce183a6540637c2644d1284190473d3e64eda
SanujaLW <sanujalw@gmail.com>
2020-05-28 11:22:28 +0530

    Uses request from environment configs

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DidSiopService } from '../did-siop.service';
+import { environment } from './../../environments/environment';
 
 @Component({
   selector: 'app-index',
@@ -7,15 +8,12 @@ import { DidSiopService } from '../did-siop.service';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
+  did_siop_request: string;
 
-  constructor(public did_siop: DidSiopService) { }
+  constructor(public did_siop: DidSiopService) {
+    this.did_siop_request = environment.did_siop_request;
+  }
 
   ngOnInit(): void {
   }
-
-  async didSIOPLogin() {
-    let request = await this.did_siop.getRequest();
-    window.open(request);
-  }
-
 }
