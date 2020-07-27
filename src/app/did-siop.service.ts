4983e916a56ac2127d42aa47abaef962a4c4405f
SanujaLW <59592997+SanujaLW@users.noreply.github.com>
2020-07-27 16:48:08 +0530

    Used did-siop 1.4.1

--- src/app/did-siop.service.ts
+++ src/app/did-siop.service.ts
@@ -7,7 +7,7 @@ import * as DID_SIOP from 'did-siop';
 })
 export class DidSiopService {
 
-  private siop_rp: any = undefined;
+  private siop_rp: DID_SIOP.RP = undefined;
 
   async checkAndInit(){
     if(this.siop_rp === undefined){
@@ -20,19 +20,11 @@ export class DidSiopService {
           }
         )
 
-        this.siop_rp.addSigningParams(
-          'CE438802C1F0B6F12BC6E686F372D7D495BC5AA634134B4A7EA4603CB25F0964', // Private key
-          'did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83#owner', // Corresponding authentication method in RP's did document (to be used as kid value for key)
-          DID_SIOP.KEY_FORMATS.HEX, //Format in which the key is supplied. List of values is given below
-          DID_SIOP.ALGORITHMS['ES256K-R']
-        );
+        this.siop_rp.addSigningParams('CE438802C1F0B6F12BC6E686F372D7D495BC5AA634134B4A7EA4603CB25F0964'); // Private key
 
-        /* this.siop_rp.setSigningParams(
-            'CE438802C1F0B6F12BC6E686F372D7D495BC5AA634134B4A7EA4603CB28F0964', // invalid Private key
-            'did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83#owner', // Corresponding authentication method in RP's did document (to be used as kid value for key)
-            DID_SIOP.KEY_FORMATS.HEX, //Format in which the key is supplied. List of values is given below
-            DID_SIOP.ALGORITHMS['ES256K-R']
-        ); */
+        /* 
+        this.siop_rp.setSigningParams('CE438802C1F0B6F12BC6E686F372D7D495BC5AA634134B4A7EA4603CB28F0964'); // invalid Private key
+        */
     }
   }
 
