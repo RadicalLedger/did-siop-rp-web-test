75cd9d800a782f8019786f7edb1c3e4b82aa120f
SanujaLW <59592997+SanujaLW@users.noreply.github.com>
2020-08-14 17:07:31 +0530

    Added browser web stores

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DidSiopService } from '../did-siop.service';
 import { environment } from './../../environments/environment';
+import { faChrome, faFirefox, faEdge, faGoogleDrive } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-index',
@@ -9,6 +10,24 @@ import { environment } from './../../environments/environment';
 })
 export class IndexComponent implements OnInit {
   did_siop_request: string;
+  storesData = {
+    chrome: {
+      link: 'https://chrome.google.com/webstore/detail/did-siop/ondpkioapbcbamnjdimjfhaelgojblad?hl=en-US&gclid=EAIaIQobChMI8MWH0Pq46gIVig4rCh3qDwHKEAAYASAAEgJiiPD_BwE',
+      icon: faChrome
+    },
+    edge: {
+      link: 'https://microsoftedge.microsoft.com/addons/detail/didsiop/obkplhmcoocpddcaompifaciljjnclfk?hl=en-GB',
+      icon: faFirefox
+    },
+    fireFox: {
+      link: 'https://addons.mozilla.org/en-US/firefox/addon/did-siop/',
+      icon: faEdge
+    },
+    drive: {
+      link: 'https://drive.google.com/drive/folders/1vTUBK9G9A5rmZ_-ZojALi_j0oXoIqjgQ?usp=sharing',
+      icon: faGoogleDrive
+    }
+  }
 
   constructor(public did_siop: DidSiopService) {
     this.did_siop_request = environment.did_siop_request;
